54bbafbd72d512c548a1d00141b5a4e8baf9fe14
Prathyusha497 <83503431+Prathyusha497@users.noreply.github.com>
2021-05-10 10:34:44 +0530

    Add files via upload

---
+++ ASSIGNMENT3_temp_humidity_may10b.ino
@@ -0,0 +1,30 @@
+#include "DHT.h"
+
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11 
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+  dht.begin();
+}
+
+void loop() 
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  delay(1000);
+  
+  }
